49c4e537786b36cfe3b3ad74b540db9538c74f2b
yangzhen19950126 <925721864@qq.com>
2016-09-02 17:07:58 +0800

    Update and rename ResourceTest.java to Resource_PropertiesTest.java

--- src/Resource_PropertiesTest.java
+++ src/Resource_PropertiesTest.java
@@ -10,7 +10,7 @@ import java.util.ResourceBundle;
  *
  *
 */
-public class ResourceTest {
+public class Resource_PropertiesTest {
 	public static void main(String[] args) {
 		// CHINA   --> ("zh", "CN") app_zh_CN.properties
 		// CHINESE --> ("zh", "")   app_zh.properties
